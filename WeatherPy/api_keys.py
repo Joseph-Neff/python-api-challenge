e9820e8f5517e0d646ad781664869b9284a64f2f
Joseph Neff <Jneff140@gmail.com>
2022-02-02 11:53:03 -0500

    updated

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "9f380259c44976fcf2bcdee6bc0d088e"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyDt2KVIejjEZ6B_rbZ9hjkxNezABqjqJ1g"
